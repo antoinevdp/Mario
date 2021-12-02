b92db8157f336d13c11da5bec1862f772698ec86
antoinevdp <antoinevdp@live.fr>
2021-12-02 11:56:22 +0100

    mob not working

--- src/mob.c
+++ src/mob.c
@@ -1,11 +1,29 @@
 #include <stdio.h>
 #include <ncurses.h>
 #include "main.h"
+#include "mob.h"
+
+
 
 void gravity();
 int canGoLeft();
 int canGoRight();
+int checkPossibleMove(int y,int x);
 
 void moveMob(){
+    while(canGoLeft())
+    mvprintw()
+}
 
+int canGoRight(){
+    int somme = 0;
+    for (int i = 0; i < MOB_DIMENSION; ++i) somme += checkPossibleMove(player_y - i, player_x+1);
+    if(somme != 0) return 0;
+    else return 1;
+}
+int canGoLeft(){
+    int somme = 0;
+    for (int i = 0; i < MOB_DIMENSION; ++i)  somme += checkPossibleMove(player_y - i, player_x-3);
+    if(somme != 0) return 0;
+    else return 1;
 }
---
+++ src/mob.h
@@ -0,0 +1,10 @@
+//
+// Created by tonio on 02/12/2021.
+//
+
+#ifndef PROJET_C_MOB_H
+#define PROJET_C_MOB_H
+
+#define MOB_DIMENSION   3
+
+#endif //PROJET_C_MOB_H
